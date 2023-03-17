30afe65ed194995207563d11dcecb4a0fa74e133
conor-b1995 <conor_carricktown@hotmail.com>
2023-03-17 22:54:43 +0000

    Created the logic for adding clubs with degrees to the shopping bag

--- bag/views.py
+++ bag/views.py
@@ -12,12 +12,25 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    degrees = None
+
+    if 'club_degrees' in request.POST:
+        degrees = request.POST['club_degrees']
     bag = request.session.get('bag', {})
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+    if degrees:
+        if item_id in list(bag.keys()):
+            if degrees in bag[item_id]['clubs_by_degrees'].keys():
+                bag[item_id]['clubs_by_degrees'][degrees] += quantity
+            else:
+                bag[item_id]['clubs_by_degrees'][degrees] = quantity
+        else:
+            bag[item_id] = {'clubs_by_degrees': {degrees: quantity}}
     else:
-        bag[item_id] = quantity
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
 
     request.session['bag'] = bag
 
